a38030b1edc15b6be036bf8e7c07ba2bf2af291d
LeroyAndrade <leroy_andrade@hotmail.com>
2024-01-05 02:25:35 +0100

    Practicum III Junit te veel problemen
    Tussentijdse upload

---
+++ practicum3jUnit/src/main/java/org/example/Main.java
@@ -0,0 +1,60 @@
+package org.example;
+//https://www.javatpoint.com/java-string-format
+
+public class Main {
+
+
+    public static class Voetbalclub {
+        private String naam;
+        private int aantalGewonnen;
+        private int aantalGelijk;
+        private int aantalVerloren;
+
+
+        public Voetbalclub(String nm) {
+            naam = nm;
+        }
+
+        public void verwerkResultaat(char ch) {
+            if (ch == 'w')
+                aantalGewonnen +=1 ;
+            if (ch == 'g')
+                aantalGelijk +=1;
+            if (ch == 'v')
+                aantalVerloren +=1;
+        }
+
+        public int aantalGespeeld(){
+            return aantalGewonnen + aantalGelijk + aantalVerloren;
+        }
+
+        public int aantalPunten(){
+            return (3*aantalGewonnen) + (aantalGelijk);
+        }
+
+        public String toString() {
+            return String.format("%s %d %d %d %d %d", naam, aantalGespeeld(), aantalGewonnen, aantalGelijk, aantalVerloren, aantalPunten());
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Voetbalclub ajx = new Voetbalclub("Ajax      ");
+        Voetbalclub feij = new Voetbalclub("Feijenoord");
+
+        feij.verwerkResultaat('w');
+        feij.verwerkResultaat('w');
+        feij.verwerkResultaat('w');
+        feij.verwerkResultaat('g');
+        ajx.verwerkResultaat('g');
+
+        feij.verwerkResultaat('g');
+
+        System.out.println("Feijenoord punten: " + feij.aantalPunten());
+        System.out.println("Ajax gespeeld: " + ajx.aantalGespeeld() + "\n");
+
+
+        System.out.println(ajx);
+        System.out.println(feij);
+    }
+}
---
+++ practicum3jUnit/src/test/java/Test.java
@@ -0,0 +1,19 @@
+import static org.junit.Assert.*;  // for assertions like assertEquals, assertTrue, etc.
+import org.junit.Before;            // for setup methods
+import org.junit.Test;              // for defining test methods
+import org.junit.After;             // for cleanup methods
+import org.junit.runner.RunWith;     // for specifying a test runner
+import org.junit.runners.JUnit4;    // a test runner for JUnit 4
+
+public class Test {
+
+    public static int myNum = 5000;
+    public static class VoetbalclubHeeftNaam {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(ajx);
+
+    }
+}
